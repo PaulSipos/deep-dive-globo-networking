e469acde2400e8b7b6de9ddb2084ce95f5ae2c40
Paul Sipos <paul.sipos@bertelsmann.de>
2025-12-16 21:41:01 +0200

    Add workspace tag and make variables required

--- resources.tf
+++ resources.tf
@@ -17,10 +17,10 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
-    Environment = var.environment
-    Terraform   = "true"
-    BillingCode = var.billing_code
-    Workspace   = terraform.workspace
+    Environment        = var.environment
+    ManagedByTerraform = "true"
+    BillingCode        = var.billing_code
+    Workspace          = terraform.workspace
   }
 }
 
